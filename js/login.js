dba7ad9d79b3c064bfa7c131e9c8e9953e1db0ed
sohanmollahbd <sohanmollahbd@gmail.com>
2024-09-21 12:05:46 +0600

    added money button and form

--- js/login.js
+++ js/login.js
@@ -11,7 +11,20 @@ document.getElementById('button-login').addEventListener('click', function(event
    //step 2: prevent default behaviors (prevent reloading browsers)
    event.preventDefault();// vejal to beginners
 
-   //step 3: get the phone number
+   //step 3: get the phone number and pin 
    const phoneNumber = document.getElementById('phone-number').value;
-   console.log(phoneNumber);
+   const pinNumber = document.getElementById('input-pin').value;
+   console.log(phoneNumber, pinNumber);
+
+   // step-4: validate phone and Pin
+        // this is temporary. You should not do like this.
+        if(phoneNumber=== '5' && pinNumber === '1234'){
+            // step -5: allow user to use the website
+
+            console.log('you are logged in');
+            window.location.href = '/home.html';
+        }
+        else{
+            alert('Wrong phone number or Pin');
+        }
 })
